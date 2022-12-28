693f2a983ca1ebe5c117c7c754942a7e951a4804
Igor <39706964+whitewhitefang@users.noreply.github.com>
2022-12-28 18:59:03 +0300

    Updates

---
+++ precache-manifest.6850c8aac40ac45f40828cb5f821b59f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "332e27a4b558c7f27d84",
+    "url": "/lotus/static/css/main.ef3c3b2a.chunk.css"
+  },
+  {
+    "revision": "332e27a4b558c7f27d84",
+    "url": "/lotus/static/js/main.6eec5ca5.chunk.js"
+  },
+  {
+    "revision": "607afd15ccf8f46b6d69",
+    "url": "/lotus/static/js/runtime~main.4339cae2.js"
+  },
+  {
+    "revision": "6f302a3aa643b5847b2b",
+    "url": "/lotus/static/js/2.e6d20c9b.chunk.js"
+  },
+  {
+    "revision": "3137927cc911705ecc74759b1fc536a3",
+    "url": "/lotus/static/media/clock.3137927c.gif"
+  },
+  {
+    "revision": "4699985c7f0b7d2bc95de0baf45f0351",
+    "url": "/lotus/index.html"
+  }
+];
