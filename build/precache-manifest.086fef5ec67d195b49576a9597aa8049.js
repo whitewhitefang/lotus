fc2ee2d2b65e69e6e8c48d5e05426eaea12cc80a
Igor <39706964+whitewhitefang@users.noreply.github.com>
2022-12-28 18:25:12 +0300

    no context build version

---
+++ build/precache-manifest.086fef5ec67d195b49576a9597aa8049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "332e27a4b558c7f27d84",
+    "url": "/static/css/main.ef3c3b2a.chunk.css"
+  },
+  {
+    "revision": "332e27a4b558c7f27d84",
+    "url": "/static/js/main.6eec5ca5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f302a3aa643b5847b2b",
+    "url": "/static/js/2.e6d20c9b.chunk.js"
+  },
+  {
+    "revision": "3137927cc911705ecc74759b1fc536a3",
+    "url": "/static/media/clock.3137927c.gif"
+  },
+  {
+    "revision": "c04aefd1d509f940c8dd94f01d3fb03a",
+    "url": "/index.html"
+  }
+];
